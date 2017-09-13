fd7acc47f4a88e5b434568eb01fce976109b4f64
cwonokay <cwonokay@gmail.com>
2017-09-13 16:23:58 -0400

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,49 @@
+//public class Main {
+//    public static void main (String[] args){
+//        GamePiece myGamePiece = new GamePiece();
+//        myGamePiece.move(9,3);
+//
+//        System.out.println(myGamePiece);
+//        System.out.println(myGamePiece.isFrozen());
+//        myGamePiece.freeze();
+//        System.out.println(myGamePiece.isFrozen());
+//        myGamePiece.unfreeze();
+//        myGamePiece.move(3,3);
+//
+//
+//    }
+
+//}
 public class Main {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         GamePiece myGamePiece = new GamePiece();
-        myGamePiece.move(9,3);
-
-        System.out.println(myGamePiece);
-        System.out.println(myGamePiece.isFrozen());
+//        Test for move
+        myGamePiece.move(5,2);
+        if(myGamePiece.getPositionX() == 5 && myGamePiece.getPositionY()==2){
+            System.out.println("SUCCESS: You moved!!");
+        } else {
+            System.out.println("FAILURE: You didn't moved");
+        }
         myGamePiece.freeze();
-        System.out.println(myGamePiece.isFrozen());
-        myGamePiece.unfreeze();
-        myGamePiece.move(3,3);
+        if (myGamePiece.isFrozen() == true){
+            System.out.println("SUCCESS: Your Frozen");
+        } else {
+            System.out.println("FAILURE: Your Not Frozen");
+        }
 
+        // Test that the default color is what we expect
+        if( myGamePiece.getColor() == null){
+            System.out.println("SUCCESS: The default color is NULL");
+        } else {
+            System.out.println("FAILURE: The default color is not NULL");
+        }
 
+        // Test that we can change the color to something else
+        myGamePiece.setColor("Black");
+        if( myGamePiece.getColor() == "Black"){
+            System.out.println("SUCCESS: The color can be set properly");
+        } else {
+            System.out.println("FAILURE: The color is not black");
+        }
     }
-
 }
